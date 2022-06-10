1e0c04589823f9fd83c46086d37dd513fcaf4295
Walid Hamza SAADAOUI <hamza.w@gmail.com>
2022-06-10 17:10:01 +0200

    t2-micro et timeout de 20 min

---
+++ essai-10-06-22/t2-timeout.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "harshicorp/aws"
+        }
+    }
+}
+
+resource "aws_instance" "Dido-Test" {
+    instance_type = "t2.micro"
+    ami = "ami-04e905a52ec8010b2"
+    
+    tags = {
+        Name = "dido_instance"
+    }
+    credit_specification {
+    cpu_credits = "standard" #soit standard ou bien unlimited (t2micro est standard)
+  }
+    timeouts {
+    delete = "20m"
+    }
+}
