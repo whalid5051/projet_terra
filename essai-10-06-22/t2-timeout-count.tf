9cf0eae013500ffdfbb64ab3658c9e679b98e2a8
Walid Hamza SAADAOUI <hamza.w@gmail.com>
2022-06-20 10:55:37 +0200

    Creation instance t2 sur AWS pour tester ansible

--- essai-10-06-22/t2-timeout-count.tf
+++ essai-10-06-22/t2-timeout-count.tf
@@ -9,14 +9,14 @@ terraform {
 resource "aws_instance" "Dido-Test" {
     instance_type = "t2.micro"
     ami = "ami-04e905a52ec8010b2"
-    count = 5     
+    count = 3     
     tags = {
         Name = "dido_instance[count.index]"
     }
     credit_specification {
     cpu_credits = "standard" #soit standard ou bien unlimited (t2micro est standard)
   }
-    timeouts {
-    delete = "20m"
-    }
+#    timeouts {
+#    delete = "20m"
+#    }
 }
